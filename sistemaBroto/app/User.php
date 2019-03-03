24be0d189556cea83d0e8a46b4661f3f45ac72e5
CarlosEduardo12 <carlosdudujunior@hotmail.com>
2019-03-03 16:08:28 -0300

    funcionario

--- sistemaBroto/app/User.php
+++ sistemaBroto/app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','funcionario',
+        'name', 'email', 'password','funcionario','idFunc',
     ];
 
     /**
