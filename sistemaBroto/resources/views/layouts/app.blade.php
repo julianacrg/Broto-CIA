f925250a59c66fedab2cf7976cc18bb03be58533
Juliana <julianacrgomes@gmail.com>
2019-03-18 22:58:07 -0300

    Retirada do menu superior

--- sistemaBroto/resources/views/layouts/app.blade.php
+++ sistemaBroto/resources/views/layouts/app.blade.php
@@ -41,8 +41,9 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
 
-
+                    
                         @guest
+                        <!--
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
@@ -52,6 +53,7 @@
                            <li class="nav-item">
                                 <a class="nav-link" href="{{ route('register') }}">{{ __('Registre-se') }}</a>
                             </li>
+                        -->
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
